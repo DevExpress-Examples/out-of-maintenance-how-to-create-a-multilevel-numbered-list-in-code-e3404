6bc540ca7f7e3696afe945b5ee2d4c7ad506a75f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/NumberedListExample/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace NumberingListExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richEditControl1 = new DevExpress.XtraRichEdit.RichEditControl();
+            this.btnSimpleNumberedList = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richEditControl1
+            // 
+            this.richEditControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.richEditControl1.Location = new System.Drawing.Point(0, 0);
+            this.richEditControl1.Name = "richEditControl1";
+            this.richEditControl1.Size = new System.Drawing.Size(532, 562);
+            this.richEditControl1.TabIndex = 0;
+            this.richEditControl1.Text = "richEditControl1";
+            // 
+            // btnSimpleNumberedList
+            // 
+            this.btnSimpleNumberedList.Location = new System.Drawing.Point(541, 12);
+            this.btnSimpleNumberedList.Name = "btnSimpleNumberedList";
+            this.btnSimpleNumberedList.Size = new System.Drawing.Size(114, 23);
+            this.btnSimpleNumberedList.TabIndex = 1;
+            this.btnSimpleNumberedList.Text = "Create numbered list";
+            this.btnSimpleNumberedList.UseVisualStyleBackColor = true;
+            this.btnSimpleNumberedList.Click += new System.EventHandler(this.btnSimpleNumberedList_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(664, 562);
+            this.Controls.Add(this.btnSimpleNumberedList);
+            this.Controls.Add(this.richEditControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraRichEdit.RichEditControl richEditControl1;
+        private System.Windows.Forms.Button btnSimpleNumberedList;
+    }
+}
+
---
+++ VB/NumberedListExample/Form1.Designer.vb
@@ -0,0 +1,70 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Namespace NumberingListExample
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Me.richEditControl1 = New DevExpress.XtraRichEdit.RichEditControl()
+			Me.btnSimpleNumberedList = New System.Windows.Forms.Button()
+			Me.SuspendLayout()
+			' 
+			' richEditControl1
+			' 
+			Me.richEditControl1.Dock = System.Windows.Forms.DockStyle.Left
+			Me.richEditControl1.Location = New System.Drawing.Point(0, 0)
+			Me.richEditControl1.Name = "richEditControl1"
+			Me.richEditControl1.Size = New System.Drawing.Size(532, 562)
+			Me.richEditControl1.TabIndex = 0
+			Me.richEditControl1.Text = "richEditControl1"
+			' 
+			' btnSimpleNumberedList
+			' 
+			Me.btnSimpleNumberedList.Location = New System.Drawing.Point(541, 12)
+			Me.btnSimpleNumberedList.Name = "btnSimpleNumberedList"
+			Me.btnSimpleNumberedList.Size = New System.Drawing.Size(114, 23)
+			Me.btnSimpleNumberedList.TabIndex = 1
+			Me.btnSimpleNumberedList.Text = "Create numbered list"
+			Me.btnSimpleNumberedList.UseVisualStyleBackColor = True
+'			Me.btnSimpleNumberedList.Click += New System.EventHandler(Me.btnSimpleNumberedList_Click);
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(664, 562)
+			Me.Controls.Add(Me.btnSimpleNumberedList)
+			Me.Controls.Add(Me.richEditControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private richEditControl1 As DevExpress.XtraRichEdit.RichEditControl
+		Private WithEvents btnSimpleNumberedList As System.Windows.Forms.Button
+	End Class
+End Namespace
+
